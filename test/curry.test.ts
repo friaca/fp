c0603bdc55d1d6379deac993e7051086c3f64028
friaca <v.fria@outlook.com>
2021-06-08 23:42:26 -0300

    [change] beautifying test suite structure

--- test/curry.test.ts
+++ test/curry.test.ts
@@ -1,23 +1,29 @@
 import { curry } from '../lib/curry';
 
-test('curries functions', () => {
-  const sum = (a: number, b: number, c: number) => a + b + c;
-  const curried = curry(sum)(1);
-  const addThree = curry(sum)(1, 2);
-  const summed = curry(sum)(1, 2, 3);
+describe('curry', () => {
+  it('curries functions with multiple arguments', () => {
+    const sum = (a: number, b: number, c: number) => a + b + c;
+    const curried = curry(sum)(1);
+    const addThree = curry(sum)(1, 2);
+    const summed = curry(sum)(1, 2, 3);
 
-  const filter = curry((fn, xs) => xs.filter(fn));
-  const onlyNumbers = filter(x => typeof x === 'number');
+    expect(typeof curried).toBe("function");
+    expect(typeof addThree).toBe("function");
+    expect(addThree(10)).toBe(13);
+    expect(summed).toBe(6);
+  })
 
-  const map = curry((fn, xs) => xs.map(fn));
-  const square = map((x: number) => x * x);
+  it('curries filter', () => {
+    const filter = curry((fn, xs) => xs.filter(fn));
+    const onlyNumbers = filter(x => typeof x === 'number');
 
-  expect(typeof curried).toBe("function");
-  expect(typeof addThree).toBe("function");
-  expect(addThree(10)).toBe(13);
-  expect(summed).toBe(6);
+    expect(onlyNumbers([1, 2, 3, 'a', '12', 4])).toStrictEqual([1, 2, 3, 4]);
+  })
 
-  expect(onlyNumbers([1, 2, 3, 'a', '12', 4])).toStrictEqual([1, 2, 3, 4]);
-  
-  expect(square([1, 2, 3, 4, 5, 'a'])).toStrictEqual([1, 4, 9, 16, 25]);
+  it('curries map', () => {
+    const map = curry((fn, xs) => xs.map(fn));
+    const square = map((x: number) => x * x);
+
+    expect(square([1, 2, 3, 4, 5])).toStrictEqual([1, 4, 9, 16, 25]);
+  })
 })
